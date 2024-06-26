a0315baaeaac7896dc02a2116f6cbd510c0b9cea
Cyc <409300777@qq.com>
2024-06-26 16:11:54 +0800

    123

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,16 +1,34 @@
-<template>
+s<template>
   <div class="login-container">
-    <el-form class="login-form">
+    <el-form class="login-form" :rules="rules" :model="loginForm">
       <!--标题-->
       <div class="title-container">博客运行后台</div>
-        <el-form-item style="margin-bottom: 30px">
+        <el-form-item style="margin-bottom: 30px" prop="username">
           <!--用户名 -->
-          <el-input placeholder="请输入用户名" name="username" type="text" prefix-icon="User"></el-input>
+          <el-input placeholder="请输入用户名" name="username" type="text" prefix-icon="User" v-model="loginForm.username"></el-input>
         </el-form-item>
-      <el-form-item style="margin-bottom: 30px">
+      <el-form-item style="margin-bottom: 30px" prop="password">
           <!--密码-->
-          <el-input placeholder="请输入密码" name="password" type="text" prefix-icon="Key" suffix-icon="view"></el-input>
-        </el-form-item>
+          <el-input placeholder="请输入密码" name="password" :type="flagType" v-model="loginForm.password">
+            <!--点击眼睛跳转图标-->
+            <template #prefix>
+              <el-icon>
+                <lock/>
+              </el-icon>
+            </template>
+            <template #suffix>
+              <span @click="changeView">
+                <el-icon v-if="flag === true" style="cursor: pointer">
+                  <Hide/>
+              </el-icon>
+                <el-icon v-else-if="flag === false" style="cursor: pointer">
+                  <View/>
+              </el-icon>
+              </span>
+            </template>
+          </el-input>
+
+      </el-form-item>
         <el-row class="button-cs">
         <!--登陆按钮-->
         <el-button  type="primary" style="width: 100px" >登陆</el-button>
@@ -22,7 +40,23 @@
 </template>
 
 <script setup>
+import { ref} from 'vue'
+import {Hide, Lock, View} from "@element-plus/icons-vue";
+import { ElForm } from 'element-plus';
+const loginForm = ref({})
+//表单验证
+const rules={
+    username:[{required:true,message:"请输入用户名",trigger:"blue"}],
+    password:[{required:true,message:"请输入密码",trigger:"blue"}],
+  }
 
+//切换眼睛事件
+const flagType = ref("password")
+const flag = ref(true)
+const changeView = () =>{
+  flag.value =!flag.value
+  flagType.value= flag.value ?"password":"text"
+}
 </script>
 <style lang="scss" scoped>
 .login-container{
---
+++ utils/request.js
@@ -0,0 +1,23 @@
+/**
+ * axios封装
+ * **/
+
+import axios from "axios"
+//创建axios
+const service = axios.create({
+  baseURL: import.meta.env.VUE_APP_BASE_API,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+})
+//核心函数
+function request(options){
+    options.method = options.method || 'get'
+    if(options.method.toLowerCase() === 'get'){
+        options.params=options.data
+    }
+    service.defaults.baseURL=import.meta.env.VUE_APP_BASE_API
+    return service(options)
+}
+export default request
